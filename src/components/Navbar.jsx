8b50cecfbeb3bc15162eab0922b8ac19e1d7d17b
anvnh <anvo20052@gmail.com>
2024-03-18 23:09:53 +0700

    Finished Hero and Stats

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,7 @@ import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false);
-    return (
+    const [toggle, setToggle] = useState(false); return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
             <img src={logo} alt="hoobank" className="w=[124px] h-[32px]" />
             <ul className="list-none sm:flex hidden justify-end items-center flex-1">
@@ -17,13 +16,11 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
-
             <div className="sm:hidden flex flex-1 justify-end items-center">
                 <img src={toggle ? close : menu} alt="menu"
                     className="w-[28px] h-[28px] object-contain"
                     onClick={() => setToggle((prev) => !prev)}
                 />
-
                 <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
                     <ul className='list-none flex justify-end items-start flex-1 flex-col'>
                         {navLinks.map((nav, index) => (
